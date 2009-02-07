ada528d7d02521bf081ec75a0439432a3c78bf17
Hector E. Gomez Morales <hectoregm@gmail.com>
2009-02-07 03:02:28 -0600

    Minor change to paths.

--- init.el
+++ init.el
@@ -15,8 +15,8 @@
                     (or (buffer-file-name) load-file-name)))
 
 (add-to-list 'load-path dotfiles-dir)
-(add-to-list 'load-path (concat dotfiles-dir "/elpa-to-submit"))
-(add-to-list 'load-path (concat dotfiles-dir "/elpa-to-submit/jabber"))
+(add-to-list 'load-path (concat dotfiles-dir "elpa-to-submit"))
+(add-to-list 'load-path (concat dotfiles-dir "elpa-to-submit/jabber"))
 
 (setq autoload-file (concat dotfiles-dir "loaddefs.el"))
 (setq package-user-dir (concat dotfiles-dir "elpa"))
