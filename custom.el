4505f3d1eec72619e6bafbe2c4043ed82a74206e
Pavel David Sulca Cavero <huaccra@gmail.com>
2009-03-06 23:10:57 -0600

    Changed font size.

--- custom.el
+++ custom.el
@@ -3,6 +3,7 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
+ '(blink-cursor-mode nil)
  '(ecb-auto-activate nil)
  '(ecb-kill-buffer-clears-history (quote auto))
  '(ecb-layout-window-sizes (quote (("left8" (0.1773049645390071 . 0.30434782608695654) (0.1773049645390071 . 0.21739130434782608) (0.1773049645390071 . 0.2826086956521739) (0.1773049645390071 . 0.17391304347826086)))))
@@ -12,11 +13,13 @@
  '(ecb-source-path (quote (("~/Projects" "Projects"))))
  '(ecb-tip-of-the-day nil)
  '(jde-jdk-registry (quote (("1.6.0.12" . "/opt/sun-jdk-1.6.0.12"))))
+ '(menu-bar-mode t)
  '(scroll-bar-mode (quote right))
+ '(show-paren-mode t)
  '(untabify-exclude-list (quote (makefile-mode makefile-bsdmake-mode change-log-mode csharp-mode php-mode css-mode html-mode c-mode "Makefile$"))))
 (custom-set-faces
   ;; custom-set-faces was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#444444" :foreground "#d3d3d3" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 172 :width normal :foundry "unknown" :family "DejaVu Sans Mono")))))
+ '(default ((t (:inherit nil :stipple nil :background "#444444" :foreground "#aaaaaa" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 98 :width normal :foundry "unknown" :family "DejaVu Sans Mono")))))
